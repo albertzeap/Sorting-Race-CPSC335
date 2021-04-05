5aa5c446010f1316d73316ebf7bf3d104500c476
Ryan Patrick <rmoneymail5@gmail.com>
2021-04-05 01:33:15 -0700

    Iterative Quicksort.

--- quicksort-manager.js
+++ quicksort-manager.js
@@ -1,17 +1,54 @@
 class QuickSortManager {      
-    *quicksortGenerator(array, min=0, max=array.length) {
-        if (max - min <= 1) return array;
-
-        let [pivot, less, greater] = [array[min], [], []];
-        for (var i = min + 1; i < max; i++) {
-            if (array[i] < pivot) less.push(array[i]);
-            else greater.push(array[i]);
-            array.splice(min, i - min + 1, ...less.concat(pivot, greater));
-            
-        }
-        yield array;
+    *quicksortGenerator(arr) {
+        // Creating an array that we'll use as a stack, using the push() and pop() functions
+        let stack = [];
         
-        yield* this.quicksortGenerator(array, min, min + less.length);
-        yield* this.quicksortGenerator(array, min + less.length + 1, max);
+        // Adding the entire initial array as an "unsorted subarray"
+        stack.push(0);
+        stack.push(arr.length - 1);
+        
+        // There isn't an explicit peek() function
+        // The loop repeats as long as we have unsorted subarrays
+        while(stack[stack.length - 1] >= 0){
+            
+            // Extracting the top unsorted subarray
+            let end = stack.pop();
+            let start = stack.pop();
+            
+            let pivotIndex = this.partition(arr, start, end);
+            
+            // If there are unsorted elements to the "left" of the pivot,
+            // we add that subarray to the stack so we can sort it later
+            if (pivotIndex - 1 > start){
+                stack.push(start);
+                stack.push(pivotIndex - 1);
+            }
+            
+            // If there are unsorted elements to the "right" of the pivot,
+            // we add that subarray to the stack so we can sort it later
+            if (pivotIndex + 1 < end){
+                stack.push(pivotIndex + 1);
+                stack.push(end);
+            }
+            yield arr;
+        }
     }
+
+        partition(arr, start, end){
+            // Taking the last element as the pivot
+            const pivotValue = arr[end];
+            let pivotIndex = start; 
+            for (let i = start; i < end; i++) {
+                if (arr[i] < pivotValue) {
+                // Swapping elements
+                [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+                // Moving to next element
+                pivotIndex++;
+                }
+            }
+            
+            // Putting the pivot value in the middle
+            [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]] 
+            return pivotIndex;
+        }
 }
